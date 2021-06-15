7365ec3576888e4f8b89454971a12917d9d33d8b
luoyancn <luoyan_cn@outlook.com>
2021-06-15 16:40:59 +0800

    Opt the color of host name

--- p10k.zsh
+++ p10k.zsh
@@ -837,13 +837,13 @@
 
   ##################################[ context: user@hostname ]##################################
   # Context color when running with privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=033
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=024
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_BACKGROUND=250
   # Context color in SSH without privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=033
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=024
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_BACKGROUND=250
   # Default context color (no privileges, no SSH).
-  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=033
+  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=024
   typeset -g POWERLEVEL9K_CONTEXT_BACKGROUND=250
 
   # Context format when running with privileges: user@hostname.
